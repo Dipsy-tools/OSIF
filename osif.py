0158d8dfc2954ea6cf6637a401357aebfee12af9
Dipsy-tools <rterangel14@gmail.com>
2025-02-02 02:02:24 +0700

    Update osif.py

--- osif.py
+++ osif.py
@@ -1,75 +1,64 @@
-show_program()
-###################################################################
-#                        Import Module
-import json , sys , hashlib , os , time , marshal, getpass
-###################################################################
-'''
-     Jangan Direcode ya bosku , tinggal make apa susahnya sih
-'''
-###################################################################
-#                             COLOR
+
+# Import Module
+import json, sys, hashlib, os, time, marshal, getpass
+
+# COLOR
 if sys.platform in ["linux", "linux2"]:
+    W = "\033[0m"
     G = '\033[32;1m'
     R = '\033[31;1m'
 else:
+    W = ''
     G = ''
     R = ''
-###################################################################
-#                      Exception
+
+# Exception Handling
 try:
-	import requests
+    import requests
 except ImportError:
-	print R + '_     _'.center(44)
-	print "o' \.=./ `o".center(44)
-	print '(o o)'.center(44)
-	print 'ooO--(_)--Ooo'.center(44)
-	print W + ' '
-	print ('O S I F').center(44)
-	print ' '
-	print "[!] Can't import module 'requests'\n"
-	sys.exit()
-####################################################################
-#                    Set Default encoding
-reload (sys)
-sys . setdefaultencoding ( 'utf8' )
-####################################################################
-#       	        I don't know
-jml = []
-jmlgetdata = []
-n = []
-####################################################################
-#                        BANNER
+    print(R + '_     _'.center(44))
+    print("o' \.=./ `o".center(44))
+    print('(o o)'.center(44))
+    print('ooO--(_)--Ooo'.center(44))
+    print(W + ' ')
+    print(('O S I F').center(44))
+    print(' ')
+    print("[!] Can't import module 'requests'\n")
+    sys.exit()
+
+# Set Default encoding
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# Banner function
 def baliho():
-	try:
-		token = open('cookie/token.log','r').read()
-		r = requests.get('https://graph.facebook.com/me?access_token=' + token)
-		a = json.loads(r.text)
-		name = a['name']
-		n.append(a['name'])
+    try:
+        token = open('cookie/token.log', 'r').read()
+        r = requests.get(f'https://graph.facebook.com/me?access_token={token}')
+        a = json.loads(r.text)
+        name = a['name']
+        print(R + '_     _'.center(44))
+        print("o' \.=./ `o".center(44))
+        print('(o o)'.center(44))
+        print('ooO--(_)--Ooo'.center(44))
+        print(' ' + W)
+        print(f'[*] {name} [*]'.center(44))
+        print(' ')
 
-		print R + '_     _'.center(44)
-		print "o' \.=./ `o".center(44)
-		print '(o o)'.center(44)
-		print 'ooO--(_)--Ooo'.center(44)
-		print ' ' + W
-		print ('[*] ' + name + ' [*]').center(44)
-		print ' '
+    except (KeyError, IOError):
+        print(R + '_     _'.center(44))
+        print("o' \.=./ `o".center(44))
+        print('(o o)'.center(44))
+        print('ooO--(_)--Ooo'.center(44))
+        print(' ' + W)
+        print(('O S I F').center(44))
+        print(f'{W}     [' + G + 'Open Source Information Facebook' + W + ']')
+        print(' ')
 
-	except (KeyError,IOError):
-		print R + '_     _'.center(44)
-		print "o' \.=./ `o".center(44)
-		print '(o o)'.center(44)
-		print 'ooO--(_)--Ooo'.center(44)
-		print ' ' + W
-		print ('O S I F').center(44)
-		print (W + '     [' + G +'Open Source Information Facebook'+ W + ']')
-		print ' '
-####################################################################
-#		    Print In terminal
+# Show program info
 def show_program():
-
-	print ('''
-                    %sINFORMATION%s
+    print(f'''
+                    {G}INFORMATION{W}
  ------------------------------------------------------
 
     Author     Dexploit 'Dex'
@@ -81,9 +70,12 @@ def show_program():
     Email      dexploit404@gmail.com
     Telegram   @Dexploit404
 
-* if you find any errors or problems , please contact
+* if you find any errors or problems, please contact
   author
-'''%(G,W))
+''')
+####################################################################
+#		    Print In terminal
+def show_program
 def info_ga():
 
 	print '''
